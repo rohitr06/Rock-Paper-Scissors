90a1ff2822d90e344870d450f789fe87792c75d8
rohitr06 <mailrrohit2004@gmail.com>
2025-02-07 23:08:46 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+import Rps from './Components/Rps';
+
+
+export default function App() {
+  return (
+  <div> <Rps/></div>
+  );
+}
+
---
+++ src/Components/Rps.jsx
@@ -0,0 +1,108 @@
+import { useState } from "react";
+
+const Rps= () =>{
+    let userWin=true;
+    var winnerMessage="";
+    const [userCount,setUserCount]=useState(0);
+    const[computerCount,setComputerCount]=useState(0);
+const choices=["Rock","Paper","Scissors"];
+const [result,setResult]=useState("Game starts");
+
+const getComputerChoice=()=>{
+   return choices[Math.floor(Math.random()*choices.length)];
+}
+
+const declareWinner=(userWin)=>{
+    if(userWin){
+        setUserCount((curr)=>curr+1);
+        return "You win";
+    }
+    else{
+        setComputerCount((curr)=>curr+1);
+        return "Computer wins";
+    }
+}
+
+
+const declareFinalWinner=(newUserCount,newComputerCount)=>{
+    if(newUserCount===5 || newComputerCount===5){
+        
+            alert(newUserCount > newComputerCount ? "You are the final winner!" : "Computer is the final winner!");
+            setUserCount(0);
+            setComputerCount(0);
+        
+    }
+}
+
+const determineWinner = (userChoice, computerChoice)=>{
+if(userChoice===computerChoice)
+     winnerMessage="It is a tie";
+else{
+    
+    if(userChoice==="Rock"){
+        userWin =computerChoice==="Paper"? false:true;
+
+    } else if(userChoice==="Paper"){
+        userWin =computerChoice==="Scissors"? false:true;
+
+    } else if(userChoice==="Scissors"){
+        userWin =computerChoice==="Rock"? false:true;
+    }
+    winnerMessage=declareWinner(userWin);
+}
+
+
+
+
+declareFinalWinner(userWin?userCount+1:userCount, userWin?computerCount:computerCount+1);
+setResult(`You chose ${userChoice} and computer chose ${computerChoice}. ${winnerMessage}`);
+}
+
+
+const handleClick = (userChoice) => {
+    
+    const computerChoice= getComputerChoice();
+    determineWinner(userChoice,computerChoice);
+    
+
+}
+return(
+<div className="bg-black text-white rounded-3xl p-9  justify-center">
+    <div className=" p-6 font-mono text-5xl "><h1>Rock Paper Scissors</h1>
+    </div>
+
+    <div className="flex justify-center space-x-44 mt-12">
+       { choices.map((choice)=>(
+        <button key={choice} className="text-2xl text-white font-serif bg-orange-500 w-24 h-24 flex items-center justify-center  rounded-full hover:bg-orange-200 hover:text-black " onClick={()=>handleClick(choice)}
+        >{choice}</button>
+       )
+    )}
+
+
+       
+    </div>
+
+    <div className="text-2xl mt-12 ">
+
+<div className="flex flex-direction-col justify-center space-x-24 ">
+<div>
+    <p>{userCount}</p>
+    <h1 className="mt-7">User count</h1>
+</div>
+<div >
+    <p>{computerCount}</p>
+    <h1 className="mt-7">Computer count</h1></div>
+</div>
+    </div>
+
+    <div className="text-2xl mt-12">
+        <p>{result}</p>
+    </div>
+
+</div>
+
+);
+
+};
+
+export default Rps;
