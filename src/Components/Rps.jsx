8b817a1f0c194f3ae7067eda68d2e3c81729ebfe
rohitr06 <mailrrohit2004@gmail.com>
2025-02-08 11:05:46 +0530

    made it Responsive

--- src/Components/Rps.jsx
+++ src/Components/Rps.jsx
@@ -83,39 +83,42 @@ const handleClick = (userChoice) => {
 
 }
 return(
-<div className="bg-white text-black rounded-3xl p-9  justify-center dark:bg-black dark:text-white ">
-    <div className=" p-6 font-mono text-5xl "><h1>Rock Paper Scissors</h1>
+<div className="h-screen flex flex-col justify-center items-center bg-white dark:bg-black text-black dark:text-white p-4 sm:p-6 lg:p-9 transition-all">
+
+    <div className=" font-mono text-3xl sm:text-4xl lg:text-5xl "><h1>Rock Paper Scissors</h1>
     </div>
 
-    <div className="flex justify-center space-x-44 mt-12">
+    <div className="flex flex-wrap justify-center gap-6 sm:gap-8 mt-8">
        { choices.map((choice)=>(
-        <button key={choice} className="text-2xl text-black dark:text-white font-serif bg-orange-500 w-24 h-24 flex items-center justify-center  rounded-full hover:bg-orange-200 hover:text-black " onClick={()=>handleClick(choice)}
+        <button key={choice} className="text-l sm:text-xl lg:text-2xl text-black dark:text-white font-serif bg-indigo-600 dark:bg-orange-500 w-24 h-24 sm:w-28 sm:h-28 flex items-center justify-center  rounded-full hover:bg-indigo-400 dark:hover:bg-orange-200 hover:text-black transition-all" onClick={()=>handleClick(choice)}
         >{choice}</button>
        )
     )}
 
 
-       
+       {/* Scores */}
     </div>
 
-    <div className="text-2xl mt-12 ">
+    <div className="text-xl sm:text-2xl mt-8 w-full max-w-md ">
 
-<div className="flex flex-direction-col justify-center space-x-24 ">
+<div className="grid grid-cols-2 gap-6 text-center ">
 <div>
-    <p>{userCount}</p>
-    <h1 className="mt-7">User count</h1>
+    <p className="text-4xl">{userCount}</p>
+    <h1 className="mt-3">User count</h1>
 </div>
 <div >
-    <p>{computerCount}</p>
-    <h1 className="mt-7">Computer count</h1></div>
+    <p className="text-4xl">{computerCount}</p>
+    <h1 className="mt-3">Computer count</h1></div>
 </div>
     </div>
 
-    <div className="text-2xl mt-12">
+    <div className="text-xl sm:text-2xl mt-8 text-center max-w-mid">
         <p>{result}</p>
     </div>
 
-    <button className="absolute bottom-16 right-16 p-2 w-16 h-16 bg-neutral-800 rounded-full text-white dark:bg-white dark:text-black" onClick={toogleDarkMode}>
+
+    {/*Dark mode toggle button*/ }
+    <button className="absolute top-10 right-10 p-3 w-16 h-16 sm:h-16 s:w-16  bg-neutral-800 rounded-full text-white dark:bg-white dark:text-black shadow-lg transition-all" onClick={toogleDarkMode}>
         {darkMode?"Light":"Dark"}</button>
 
 </div>
